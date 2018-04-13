4a6253873c267a46e77237dd5db9cd907c0f7d06
Angel <angelmmed12@gmail.com>
2018-04-13 09:57:56 -0500

    set up tables

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,21 @@
+class MainController < ApplicationController
+  def index
+    
+  end
+    
+  def high_score
+    hs = HighScore.all.sort_by(&:score).last
+    hs = hs || HighScore.new
+    render json: {user: hs.name , :score: hs.score}.to_json    
+  end
+  
+  def post_score
+    HighScore.create!(high_score_params)
+    render status: 200, json: {}.to_json
+  end
+  
+  private
+  def high_score_params
+    params.require(:high_score).permit(:user, :score)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
+  get 'high_score' => 'welcome#high_score'
+  post 'high_score' => 'welcome#post_score'
 end
